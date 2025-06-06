fb72df6cc65e6c9bc65557bc7bbdb823992cad89
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2025-06-05 20:50:23 -0400

    ahora se usa el digito verificador como "grupo"

--- elipse.py
+++ elipse.py
@@ -5,19 +5,20 @@ class Elipse:
     def __init__(self, rut:str):
         self.rut = rut
         self.digitos = self._extraer_digitos()
-        self.grupo = int(''.join(map(str, self.digitos)))
+        self.verificador = int(rut[-1]) if rut[-1].isdigit() else 10    # Guarda como entero el ultimo caracter, si es K lo guarda como 10
         self.h, self.k = self.digitos[0], self.digitos[1]
         self._calcular_parametros()
 
     def _extraer_digitos(self):
+        # Extrae todos los digitos del RUT y los convierte en una lista de enteros
         numeros = ''.join(c for c in self.rut if c.isdigit())
         if len(numeros) < 8:
             raise ValueError("El rut debe tener 8 digitos")
         return list(map(int, numeros[:8]))
     
-    def _calcular_parametros(self):
+    def _calcular_parametros(self):     # Calcula los parametros a, b y orientacion de la elipse de acuerdo al RUT
         d = self.digitos
-        if self.grupo % 2 == 0:
+        if self.verificador % 2 == 0:
             self.a = d[5] + d[6]
             self.b = d[7] + d[2]
             self.orientacio = 'Horizontal' if d[3] % 2 == 0 else 'Vertical'
@@ -39,9 +40,10 @@ class Elipse:
             f"{A}(x - {self.h})² + {B}(Y - {self.k})² = {A * B}"
         )
         return expr
-    
-elipse1 = Elipse("12.345.678-9")
-elipse2 = Elipse("11.223.344-5")
+if __name__ == "__main__":
+    # Ejemplo de uso
+    elipse1 = Elipse("12.345.678-9")
+    elipse2 = Elipse("11.223.344-5")
 
-print("Ecuación canónica:", elipse1.ecuacion_canonica())
-print("Ecuación general:", elipse1.ecuacion_general())
+    print("Ecuación canónica:", elipse1.ecuacion_canonica())
+    print("Ecuación general:", elipse1.ecuacion_general())
